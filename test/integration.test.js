b61b4261df5f613070f629e754477481523392dd
Ryan Graham <r.m.graham@gmail.com>
2016-05-06 11:36:00 -0700

    update copyright notices and license

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2014,2015. All Rights Reserved.
+// Node module: strong-cached-install
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
 /*global describe, it, beforeEach */
 var exec = require('child_process').exec;
 var fs = require('fs-extra');
